8ba3ca7123d868d1c2eacb23cba7bc8e9f85e261
Mitchell Hertel <mhertel@student.cscc.edu>
2019-10-09 13:53:24 -0400

    Initial commit

---
+++ src/edu/cscc/SquareAndCountTest.java
@@ -0,0 +1,22 @@
+package edu.cscc;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SquareAndCountTest {
+
+    @Test
+    public void square() {
+        SquareAndCount testerObj = new SquareAndCount();
+        int result = testerObj.square(19);
+        Assert.assertEquals(result, 361);
+    }
+
+    @Test
+    public void countA() {
+        SquareAndCount testerObj = new SquareAndCount();
+        int result = testerObj.countA("canada");
+        Assert.assertEquals(result, 3);
+    }
+}
